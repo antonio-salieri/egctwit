6925453725f3ecf135321e174aff83fade65d439
Lazar Ivanović <laza@opencores.org>
2015-02-15 13:13:00 +0100

    Fixed routes
    Signed-off-by: Lazar Ivanović <laza@opencores.org>

--- module/EgcTweet/config/module.config.php
+++ module/EgcTweet/config/module.config.php
@@ -1,24 +1,16 @@
 <?php
-/**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
- * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
- */
-
 return array(
     'router' => array(
         'routes' => array(
             'home' => array(
                 'type' => 'Zend\Mvc\Router\Http\Literal',
                 'options' => array(
-                    'route'    => '/',
+                    'route' => '/',
                     'defaults' => array(
-			'__NAMESPACE__' => 'EgcTweet\Controller',
-			'controller' => 'Index',
-			'action' => 'index',
-                    ),
+                        '__NAMESPACE__' => 'EgcTweet\Controller',
+                        'controller' => 'Index',
+                        'action' => 'index'
+                    )
                 )
             ),
             'user' => array(
@@ -34,68 +26,67 @@ return array(
                 )
             ),
             'egc-tweet' => array(
-		'type' => 'Segment',
-		'options' => array(
-                    'route'    => '/[:controller[/:action]]',
+                'type' => 'segment',
+                'options' => array(
+                    'route' => '/[:controller[/:action]]',
                     'constraints' => array(
                         'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
-                        'action'     => '[a-zA-Z][a-zA-Z0-9_-]*',
+                        'action' => '[a-zA-Z][a-zA-Z0-9_-]*'
                     ),
-		    'defaults' => array(
-			'__NAMESPACE__' => 'EgcTweet\Controller',
-			'controller' => 'Index',
-			'action' => 'index'
-		    )
-                ),
+                    'defaults' => array(
+                        'module' => 'egc-tweet',
+                        'controller' => 'index',
+                        'action' => 'index'
+                    )
+                )
             )
-        ),
+        )
     ),
     'service_manager' => array(
         'abstract_factories' => array(
             'Zend\Cache\Service\StorageCacheAbstractServiceFactory',
-            'Zend\Log\LoggerAbstractServiceFactory',
+            'Zend\Log\LoggerAbstractServiceFactory'
         ),
         'aliases' => array(
-            'translator' => 'MvcTranslator',
-        ),
+            'translator' => 'MvcTranslator'
+        )
     ),
     'translator' => array(
         'locale' => 'en_US',
         'translation_file_patterns' => array(
             array(
-                'type'     => 'gettext',
+                'type' => 'gettext',
                 'base_dir' => __DIR__ . '/../language',
-                'pattern'  => '%s.mo',
-            ),
-        ),
+                'pattern' => '%s.mo'
+            )
+        )
     ),
     'controllers' => array(
         'invokables' => array(
-            'EgcTweet\Controller\Index' => 'EgcTweet\Controller\IndexController',
-            'EgcTweet\Controller\Profile' => 'EgcTweet\Controller\ProfileController'
-        ),
+            'index' => 'EgcTweet\Controller\IndexController',
+            'profile' => 'EgcTweet\Controller\ProfileController'
+        )
     ),
     'view_manager' => array(
         'display_not_found_reason' => true,
-        'display_exceptions'       => true,
-        'doctype'                  => 'HTML5',
-        'not_found_template'       => 'error/404',
-        'exception_template'       => 'error/index',
+        'display_exceptions' => true,
+        'doctype' => 'HTML5',
+        'not_found_template' => 'error/404',
+        'exception_template' => 'error/index',
         'template_map' => array(
-            'layout/layout'           => __DIR__ . '/../view/layout/layout.phtml',
+            'layout/layout' => __DIR__ . '/../view/layout/layout.phtml',
             'application/index/index' => __DIR__ . '/../view/egc-tweet/index/index.phtml',
-            'error/404'               => __DIR__ . '/../view/error/404.phtml',
-            'error/index'             => __DIR__ . '/../view/error/index.phtml',
+            'error/404' => __DIR__ . '/../view/error/404.phtml',
+            'error/index' => __DIR__ . '/../view/error/index.phtml'
         ),
         'template_path_stack' => array(
-            __DIR__ . '/../view',
-        ),
+            __DIR__ . '/../view'
+        )
     ),
     // Placeholder for console routes
     'console' => array(
         'router' => array(
-            'routes' => array(
-            ),
-        ),
-    ),
+            'routes' => array()
+        )
+    )
 );
