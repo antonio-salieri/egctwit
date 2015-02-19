42de0a63a2cceac9faeaafe305838d8c32dedf34
Lazar Ivanović <laza@opencores.org>
2015-02-19 07:03:43 +0100

    - Added sqlite db config in global config - Changed application name in composer.json
    Signed-off-by: Lazar Ivanović <laza@opencores.org>

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -12,5 +12,13 @@
  */
 
 return array(
-    // ...
+    'db' => array(
+        'driver'    => 'PdoSqlite',
+        'database'  => 'data/egctweet.sqlite',
+    ),
+    'service_manager' => array(
+        'factories' => array(
+            'Zend\Db\Adapter\Adapter' => 'Zend\Db\Adapter\AdapterServiceFactory'
+        ),
+    ),
 );
